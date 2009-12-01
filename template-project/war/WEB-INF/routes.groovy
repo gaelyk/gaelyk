84942d16f7858057adeb136cc2492a9320dab348
glaforge <glaforge@gmail.com>
2009-12-01 11:48:42 +0100

    Added settings in appengine-web.xml about UTF-8 encoding for templates

--- template-project/war/WEB-INF/routes.groovy
+++ template-project/war/WEB-INF/routes.groovy
@@ -1,3 +1,4 @@
+// example routes, please remove them or update them as appropriate
 get "/blog/@year/@month/@day/@title", forward: "/WEB-INF/groovy/blog.groovy?year=@year&month=@month&day=@day&title=@title"
 get "/something", redirect: "/blog/2008/10/20/something"
 get "/book/isbn/@isbn", forward: "/WEB-INF/groovy/book.groovy?isbn=@isbn", validate: { isbn ==~ /\d{9}(\d|X)/ }

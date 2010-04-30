0b68551ae619713e5555a933fdf18c1a82d2185c
glaforge <glaforge@gmail.com>
2010-04-30 18:41:51 +0200

    Using the local mail service logging capability to check emails contain the right information

--- core/src/test/groovyx/gaelyk/MailSupportTest.groovy
+++ core/src/test/groovyx/gaelyk/MailSupportTest.groovy
@@ -4,6 +4,9 @@ import com.google.appengine.tools.development.testing.LocalMailServiceTestConfig
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper
 import com.google.appengine.api.mail.MailService
 import com.google.appengine.api.mail.MailServiceFactory
+import java.util.logging.Logger
+import java.util.logging.Filter
+import java.util.logging.LogRecord
 
 /**
  * 
@@ -18,23 +21,44 @@ class MailSupportTest extends GroovyTestCase {
 
     private MailService mail
 
+    private String logResult
+
     protected void setUp() {
         super.setUp()
         // setting up the local environment
         helper.setUp()
 
         mail = MailServiceFactory.mailService
+
+        logResult = ""
+
+        Logger log = Logger.getLogger("com.google.appengine.api.mail.dev.LocalMailService")
+        log.filter = { LogRecord logRecord ->
+            logResult += logRecord.message + '\n'
+            return false
+        } as Filter
     }
 
     protected void tearDown() {
         // uninstalling the local environment
         helper.tearDown()
         super.tearDown()
+        logResult = ""
     }
 
     void testSend() {
         use (GaelykCategory) {
-            mail.send from: "glaforge@gmail.com", to: "someone@gmail.com", textBody: "hello"
+            mail.send from: "glaforge@gmail.com",
+                    to: "someone@gmail.com",
+                    textBody: "hello",
+                    subject: "new message"
         }
+
+        println logResult
+
+        assert logResult.contains("glaforge@gmail.com")
+        assert logResult.contains("someone@gmail.com")
+        assert logResult.contains("new message")
+        assert logResult.contains("hello")
     }
 }

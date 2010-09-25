dc6ceb745c960a505d1f6544e0ffb798e8f7de65
glaforge <glaforge@gmail.com>
2010-09-25 09:17:05 +0200

    Bypass all the /_ah urls Added caching for all the pages (1 hour) Reformating

--- website/war/WEB-INF/routes.groovy
+++ website/war/WEB-INF/routes.groovy
@@ -1,14 +1,18 @@
-get "/tutorial",    forward: "/tutorial.gtpl"
-get "/download",    forward: "/download.gtpl"
-get "/community",   forward: "/community.gtpl"
+get "/tutorial",    forward: "/tutorial.gtpl",  cache: 1.hour
+get "/download",    forward: "/download.gtpl",  cache: 1.hour
+get "/community",   forward: "/community.gtpl", cache: 1.hour
+
+// bypass all admin urls
+
+all "/_ah/**", ignore: true
 
 // for compatibility-sake with older allowed URLs
 
-get "/tutorial/",    forward: "/tutorial.gtpl"
-get "/download/",    forward: "/download.gtpl"
-get "/community/",   forward: "/community.gtpl"
+get "/tutorial/",    forward: "/tutorial.gtpl",     cache: 1.hour
+get "/download/",    forward: "/download.gtpl",     cache: 1.hour
+get "/community/",   forward: "/community.gtpl",    cache: 1.hour
 
-get "/tutorial/index.gtpl",    forward: "/tutorial.gtpl"
-get "/download/index.gtpl",    forward: "/download.gtpl"
-get "/community/index.gtpl",   forward: "/community.gtpl"
+get "/tutorial/index.gtpl",    forward: "/tutorial.gtpl",   cache: 1.hour
+get "/download/index.gtpl",    forward: "/download.gtpl",   cache: 1.hour
+get "/community/index.gtpl",   forward: "/community.gtpl",  cache: 1.hour
 

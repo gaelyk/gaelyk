65ad56d1f095ad22e0009c246a4d975339036f99
Guillaume Laforge <glaforge@gmail.com>
2010-12-25 16:09:24 +0100

    Add test coverage for the lazy binding used by plugins

---
+++ core/src/test/groovyx/gaelyk/plugins/LazyBindingTest.groovy
@@ -0,0 +1,22 @@
+package groovyx.gaelyk.plugins
+
+/**
+ * Test for the lazy binding used by the plugin mechanism
+ * 
+ * @author Guillaume Laforge
+ */
+class LazyBindingTest extends GroovyTestCase {
+
+    void testBinding() {
+        def binding = new LazyBinding()
+
+        binding.name = "Guillaume"
+        binding.age = 33
+
+        assert binding.name == "Guillaume"
+        assert binding.age == 33
+
+        assert binding.unknown == "unknown"
+        assert binding.nonexistant == "nonexistant"
+    }
+}

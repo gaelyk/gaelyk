ff1a40dc926412acf2eaf73dd729f484e2f6f54e
Guillaume Laforge <glaforge@gmail.com>
2010-12-29 14:51:39 +0100

    Code coverage for the queue accessor

---
+++ core/src/test/groovyx/gaelyk/QueueAccessorTest.groovy
@@ -0,0 +1,43 @@
+package groovyx.gaelyk
+
+import com.google.appengine.tools.development.testing.LocalTaskQueueTestConfig
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper
+import com.google.appengine.api.utils.SystemProperty
+import com.google.appengine.api.taskqueue.QueueFactory
+
+/**
+ * 
+ * @author Guillaume Laforge
+ */
+class QueueAccessorTest extends GroovyTestCase {
+
+    // setup the local environement stub services
+    private LocalServiceTestHelper helper = new LocalServiceTestHelper(
+            new LocalTaskQueueTestConfig(),
+    )
+
+    protected void setUp() {
+        super.setUp()
+
+        // setting up the local environment
+        helper.setUp()
+    }
+
+    protected void tearDown() {
+        // uninstalling the local environment
+        helper.tearDown()
+
+        super.tearDown()
+    }
+
+
+    void testAccessor() {
+        def queues = new QueueAccessor()
+
+        assert queues.default == QueueFactory.defaultQueue
+        assert queues.myQueue == QueueFactory.getQueue('myQueue')
+
+        assert queues['default'] == QueueFactory.defaultQueue
+        assert queues['myQueue'] == QueueFactory.getQueue('myQueue')
+    }
+}

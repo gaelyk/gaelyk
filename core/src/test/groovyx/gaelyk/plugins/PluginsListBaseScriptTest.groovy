8b80ee60e90015aa77fb4d3d4d75e2f5ea776181
Guillaume Laforge <glaforge@gmail.com>
2010-12-31 00:11:10 +0100

    Add test for the plugins list base script

---
+++ core/src/test/groovyx/gaelyk/plugins/PluginsListBaseScriptTest.groovy
@@ -0,0 +1,30 @@
+package groovyx.gaelyk.plugins
+
+import org.codehaus.groovy.control.CompilerConfiguration
+
+/**
+ * 
+ * @author Guillaume Laforge
+ */
+class PluginsListBaseScriptTest extends GroovyTestCase {
+    
+    void testBaseScriptWithInstallMethod() {
+        def config = new CompilerConfiguration()
+        config.scriptBaseClass = PluginsListBaseScript.class.name
+
+        def binding = new LazyBinding()
+
+        def shell = new GroovyShell(binding, config)
+
+        PluginsListBaseScript script = (PluginsListBaseScript) shell.parse("""
+            install jsonPlugin
+            install restPlugin
+            install scaffoldPlugin
+        """)
+        script.run()
+
+        def plugins = script.getPlugins()
+
+        assert plugins == ['jsonPlugin', 'restPlugin', 'scaffoldPlugin']
+    }
+}

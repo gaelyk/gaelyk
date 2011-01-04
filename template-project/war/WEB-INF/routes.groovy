301a59c5e5409306d8c9e6188e28cffc54b0b04b
Guillaume Laforge <glaforge@gmail.com>
2011-01-04 13:20:43 +0100

    Remove commented routes and added a route for favicon.ico

--- template-project/war/WEB-INF/routes.groovy
+++ template-project/war/WEB-INF/routes.groovy
@@ -1,11 +1,7 @@
-// example routes
-/*
-get "/blog/@year/@month/@day/@title", forward: "/WEB-INF/groovy/blog.groovy?year=@year&month=@month&day=@day&title=@title"
-get "/something", redirect: "/blog/2008/10/20/something", cache: 2.hours
-get "/book/isbn/@isbn", forward: "/WEB-INF/groovy/book.groovy?isbn=@isbn", validate: { isbn ==~ /\d{9}(\d|X)/ }
-*/
 
 // routes for the blobstore service example
 get "/upload",  forward: "/upload.gtpl"
 get "/success", forward: "/success.gtpl"
 get "/failure", forward: "/failure.gtpl"
+
+get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"

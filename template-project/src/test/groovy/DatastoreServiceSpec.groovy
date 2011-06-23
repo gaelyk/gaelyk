068e48e413e34cbeb091633d5e752a28f58bedf3
Guillaume Laforge <glaforge@gmail.com>
2011-06-23 14:03:34 +0200

    Spock test example

---
+++ template-project/src/test/groovy/DatastoreServiceSpec.groovy
@@ -0,0 +1,27 @@
+import groovyx.gaelyk.spock.*
+import com.google.appengine.api.datastore.*
+import static com.google.appengine.api.datastore.FetchOptions.Builder.*
+
+class DatastoreServiceSpec extends GaelykUnitSpec {
+
+    def setup() {
+        groovlet 'dataStoreGroovlet.groovy'
+    }
+
+    def "the datastore is used from within the groovlet"() {
+        given: "the initialised groovlet is invoked and data is persisted"
+        dataStoreGroovlet.get()
+
+        when: "the datastore is queried for data"
+        def query = new Query("person")
+        query.addFilter("firstname", Query.FilterOperator.EQUAL, "Marco")
+        def preparedQuery = datastore.prepare(query)
+        def entities = preparedQuery.asList(withLimit(1))
+
+        then: "the persisted data is found in the datastore"
+        def person = entities[0]
+        person.firstname == 'Bart'
+        person.lastname == 'Simpson'
+    }
+
+}

a2b3b0f0ccf50cf0e405f090b977088a7df407dd
Guillaume Laforge <glaforge@gmail.com>
2012-06-30 22:01:57 +0200

    Tabs to spaces

--- website/war/WEB-INF/groovy/latestcommits.groovy
+++ website/war/WEB-INF/groovy/latestcommits.groovy
@@ -9,13 +9,13 @@ def result = slurper.parseText(url.text)
 def sdf = new SDF("yyyy-MM-dd'T'HH:mm:ssz", Locale.US)
 
 html.ul {
-	result.commit.each { commit ->
-		li {
-			a href: "https://github.com${commit.url}", commit.message
-			def time = commit.'committed-date'.text().replaceAll(/(-|\+)(\d\d):(\d\d)/, '$1$2$3')
-			def prettyTime = new PrettyTime().format(sdf.parse(time))
-			i "committed ${prettyTime}"
+    result.commit.each { commit ->
+        li {
+            a href: "https://github.com${commit.url}", commit.message
+            def time = commit.'committed-date'.text().replaceAll(/(-|\+)(\d\d):(\d\d)/, '$1$2$3')
+            def prettyTime = new PrettyTime().format(sdf.parse(time))
+            i "committed ${prettyTime}"
             i "by ${commit.author.name}"
-		}
-	}	
+        }
+    }
 }

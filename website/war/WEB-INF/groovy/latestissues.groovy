aa8fac3145f5df90243ff792231a33e877c057fa
Guillaume Laforge <glaforge@gmail.com>
2012-06-30 22:02:39 +0200

    Tabs to spaces

--- website/war/WEB-INF/groovy/latestissues.groovy
+++ website/war/WEB-INF/groovy/latestissues.groovy
@@ -8,14 +8,14 @@ def struct = new JsonSlurper().parseText(content)
 def sdf = new SDF("yyyy/MM/dd HH:mm:ss Z", Locale.US)
 
 html.ul {
-	struct.issues.each { issue ->
-		li {
-			a href: "http://github.com/glaforge/gaelyk/issues#issue/${issue.number}", issue.title
-//			2009/07/20 01:41:25 -0700
-			def prettyTime = new PrettyTime().format(sdf.parse(issue.created_at))
-			i " opened ${prettyTime}"
+    struct.issues.each { issue ->
+        li {
+            a href: "http://github.com/glaforge/gaelyk/issues#issue/${issue.number}", issue.title
+//          2009/07/20 01:41:25 -0700
+            def prettyTime = new PrettyTime().format(sdf.parse(issue.created_at))
+            i " opened ${prettyTime}"
             i "by ${issue.user}"
-		}
-	}
+        }
+    }
 }
 

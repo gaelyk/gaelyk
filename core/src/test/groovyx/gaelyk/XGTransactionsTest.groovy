350649df2cb97af6a5672970592cffa009db0303
Robert Fischer <robert.fischer@smokejumperit.com>
2012-08-11 19:20:42 -0400

    merged xg transactions

---
+++ core/src/test/groovyx/gaelyk/XGTransactionsTest.groovy
@@ -0,0 +1,57 @@
+package groovyx.gaelyk
+
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig
+import com.google.appengine.api.datastore.DatastoreServiceFactory
+import com.google.appengine.api.datastore.Entity
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query
+import com.google.appengine.api.datastore.Transaction
+import com.google.appengine.api.datastore.AsyncDatastoreService
+import com.google.appengine.api.datastore.Key
+import java.util.concurrent.Future
+import com.google.appengine.api.datastore.KeyFactory
+
+/**
+ * @author Guillaume Laforge
+ */
+class XGTransactionsTest extends GroovyTestCase {
+    // setup the local environment stub services
+    private LocalServiceTestHelper helper = makeHelper() 
+
+    static LocalServiceTestHelper makeHelper() {
+			def helperConfig = new LocalDatastoreServiceTestConfig()
+            helperConfig.defaultHighRepJobPolicyRandomSeed = 1L
+            // helperConfig.defaultHighRepJobPolicyUnappliedJobPercentage = 0.0f
+			helperConfig.defaultHighRepJobPolicyUnappliedJobPercentage = 100f
+			return new LocalServiceTestHelper(helperConfig)
+    }
+
+    protected void setUp() {
+        super.setUp()
+        helper.setUp()
+    }
+
+    protected void tearDown() {
+        helper.tearDown()
+        super.tearDown()
+    }
+
+	void testDatastoreTransactionWithBuilderOptions() {
+		def datastore = DatastoreServiceFactory.datastoreService
+		use(GaelykCategory) {
+			datastore.withTransaction(true) {
+                new Entity('foo').save()
+                new Entity('bar').save()
+			}
+			shouldFail {
+                datastore.withTransaction(false){
+                    new Entity('foo').save()
+                    new Entity('bar').save()
+                }
+			}
+            assert !DatastoreServiceFactory.datastoreService.activeTransactions
+			
+		}
+	}
+}

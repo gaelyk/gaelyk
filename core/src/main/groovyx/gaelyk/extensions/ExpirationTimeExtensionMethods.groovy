83dfc65c7806fb0f32a5bdb4b1ac115cd84a9f40
Guillaume Laforge <glaforge@gmail.com>
2012-09-11 12:04:34 +0200

    Statically compile ExpirationTimeExtensionMethods

--- core/src/main/groovyx/gaelyk/extensions/ExpirationTimeExtensionMethods.groovy
+++ core/src/main/groovyx/gaelyk/extensions/ExpirationTimeExtensionMethods.groovy
@@ -15,11 +15,14 @@
  */
 package groovyx.gaelyk.extensions
 
+import groovy.transform.CompileStatic
+
 /**
  * Extension methods for time duration handling, for the caching expiration definition
  *
  * @author Guillaume Laforge
  */
+@CompileStatic
 class ExpirationTimeExtensionMethods {
     /**
      * Method to support the notation: <code>10.seconds</code> in the URL routing configuration file

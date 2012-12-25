3fdc9c1ef3c6756f388e2b1c00b19c70d3078a37
Vladimir Orany <vladimir@orany.cz>
2012-12-25 10:33:46 +0100

    precompilation helper class

---
+++ core/src/main/groovyx/grout/tools/GroutTools.java
@@ -0,0 +1,15 @@
+package groovyx.grout.tools;
+
+/**
+ * Grout tools support. 
+ * 
+ * Contains method which can be used by precompilation tasks.
+ * 
+ * @author Vladimir Orany
+ *
+ */
+public class GroutTools {
+    
+    private GroutTools() { }
+
+}

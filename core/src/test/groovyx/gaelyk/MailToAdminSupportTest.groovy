6c89ff16369bede6a80e56ec37e419371fa67bbb
Vladimir Orany <vladimir@orany.cz>
2013-05-17 09:02:07 +0200

    fixed send to admin test

--- core/src/test/groovyx/gaelyk/MailToAdminSupportTest.groovy
+++ core/src/test/groovyx/gaelyk/MailToAdminSupportTest.groovy
@@ -15,22 +15,15 @@ import java.util.logging.Logger
 class MailToAdminSupportTest extends GroovyTestCase {
 
     // setup the local environment with a mail service stub
+    private LocalMailServiceTestConfig localMSConfig = new LocalMailServiceTestConfig().setLogMailBody(true)//.setLogMailLevel(Level.INFO)
     private LocalServiceTestHelper helper = new LocalServiceTestHelper(
-            new LocalMailServiceTestConfig().setLogMailBody(true)//.setLogMailLevel(Level.INFO)
+        localMSConfig
     )
-
-    private String logResult = ""
 
     protected void setUp() {
         super.setUp()
         // setting up the local environment
         helper.setUp()
-
-        Logger log = Logger.getLogger("com.google.appengine.api.mail.dev.LocalMailService")
-        log.filter = { LogRecord logRecord ->
-            logResult += logRecord.message + '\n'
-            return false
-        } as Filter
     }
 
     protected void tearDown() {
@@ -45,11 +38,10 @@ class MailToAdminSupportTest extends GroovyTestCase {
         mail.sendToAdmins from: "glaforge@gmail.com",
                 textBody: "hello admin",
                 subject: "new message"
-
-        println logResult
-
-        assert logResult.contains("glaforge@gmail.com")
-        assert logResult.contains("new message")
-        assert logResult.contains("hello admin")
+        
+        assert localMSConfig.localMailService.sentMessages.size() == 1
+        assert localMSConfig.localMailService.sentMessages[0].sender == "glaforge@gmail.com"
+        assert localMSConfig.localMailService.sentMessages[0].subject == "new message"
+        assert localMSConfig.localMailService.sentMessages[0].textBody == "hello admin"
     }
 }
